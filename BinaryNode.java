d9de1823700bd904c5ace11cdb9dcf422e7ca8d4
mrgoose25 <70932204+mrgoose25@users.noreply.github.com>
2020-11-10 00:43:32 -0700

    Create BinaryNode.java

---
+++ BinaryNode.java
@@ -0,0 +1,11 @@
+public class BinaryNode {
+    BinaryNode left;
+    BinaryNode right;
+    int info;
+
+    BinaryNode(BinaryNode l,int in,BinaryNode r){
+        left= l;
+        info = in;
+        right = r;
+    }
+}
